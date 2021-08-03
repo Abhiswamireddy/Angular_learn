d83c0942653afc3f794c44d491dd5ecf8fe7e8fe
Abhiswamireddy <Abhiswamireddy@gmail.com>
2021-08-03 14:05:35 +0530

    check-in changes

--- Heroes-app/src/app/Datalist-heroes.ts
+++ Heroes-app/src/app/Datalist-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 1, name: 'Hero_1' },
-  { id: 2, name: 'Hero_2' },
-  { id: 3, name: 'Hero_3' },
-  { id: 4, name: 'Hero_4' },
-  { id: 5, name: 'Hero_5' },
-  { id: 6, name: 'Hero_6' },
-  { id: 7, name: 'Hero_7' },
-  { id: 8, name: 'Hero_8' },
-  { id: 9, name: 'Hero_9' },
-  { id: 10, name: 'Hero_10' }
+  { id: 1, name: 'Hero_1',Color:'Blue' },
+  { id: 2, name: 'Hero_2' ,Color:'Blue'},
+  { id: 3, name: 'Hero_3',Color:'Red' },
+  { id: 4, name: 'Hero_4' ,Color:'Blue'},
+  { id: 5, name: 'Hero_5' ,Color:'Red'},
+  { id: 6, name: 'Hero_6',Color:'Blue' },
+  { id: 7, name: 'Hero_7',Color:'Blue' },
+  { id: 8, name: 'Hero_8' ,Color:'Red'},
+  { id: 9, name: 'Hero_9',Color:'Green' },
+  { id: 10, name: 'Hero_10',Color:'Green' }
 ];
